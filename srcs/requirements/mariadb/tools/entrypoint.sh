0b3dceceffe57bfa94453a028ad85969d42c1373
Vinicius Oliveira <vinicius@myriad.codes>
2023-07-31 17:59:39 -0300

    fix: do not pass environment variables as build args

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-mysqld --user=mysql --bootstrap << EOF
+mysqld --user=mysql --bootstrap <<EOF
 USE mysql;
 FLUSH PRIVILEGES;
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 
 DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.db WHERE Db='test';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'; 
 
 FLUSH PRIVILEGES;
 EOF
+
+exec mysqld_safe
